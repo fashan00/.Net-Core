755819e839966d2976407e556cdfa2805b5753db
WHQ\10604308 <Yishan_Chen@wistron.com>
2018-06-05 11:52:35 +0800

    Static Files

--- MyWebAPI/Startup.cs
+++ MyWebAPI/Startup.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
@@ -37,6 +40,28 @@ namespace MyWebAPI {
             }
 
             app.UseMvc ();
+
+            // UseDefaultFiles 必須註冊在 UseStaticFiles 之前。
+            // 如果先註冊 UseStaticFiles，當 URL 是 / 時，UseStaticFiles 找不到該檔案，就會直接回傳找不到；所以就沒有機會進到 UseDefaultFiles。
+
+            // 啟用預設檔案: http://localhost:5000/ 可以自動指向到 index.html
+            app.UseDefaultFiles ();
+            // 啟用靜態檔案: http://localhost:5000/index.html
+            app.UseStaticFiles ();
+            // 啟用指定目錄: http://localhost:5000/third-party/index.html
+            app.UseStaticFiles (new StaticFileOptions () {
+                FileProvider = new PhysicalFileProvider (
+                        Path.Combine (env.ContentRootPath, @"wwwpublic")),
+                    RequestPath = new PathString ("/third-party")
+            });
+            // 啟用檔案清單: http://localhost:5000/StaticFiles
+            app.UseFileServer (new FileServerOptions () {
+                FileProvider = new PhysicalFileProvider (
+                        Path.Combine (env.ContentRootPath, @"bin")
+                    ),
+                    RequestPath = new PathString ("/StaticFiles"),
+                    EnableDirectoryBrowsing = true
+            });
         }
     }
 }
