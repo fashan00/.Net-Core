e444388d65154485bb5460c1c3417344dbe13680
WHQ\10604308 <Yishan_Chen@wistron.com>
2018-06-07 16:58:38 +0800

    Model Binding

--- MyMVC/Controllers/HomeController.cs
+++ MyMVC/Controllers/HomeController.cs
@@ -4,55 +4,44 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using MyMVC.Models;
 
 namespace MyMVC.Controllers {
     public class HomeController : Controller {
-        // public IActionResult Index()
-        // {
-        //     return View();
-        // }
 
-        // public IActionResult About()
-        // {
-        //     ViewData["Message"] = "Your application description page.";
-
-        //     return View();
-        // }
-
-        // public IActionResult Contact()
-        // {
-        //     ViewData["Message"] = "Your contact page.";
-
-        //     return View();
-        // }
-
-        // public IActionResult Error()
-        // {
-        //     return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        // }
-
-        public IActionResult Index () {
-            var user = new UserModel ();
-            return View (model: user);
+        // Route: http://localhost:5000/Home/Index/2
+        // Query: http://localhost:5000/Home/Index?id=1
+        // Form(Body) > Route > Query
+        public IActionResult Index (string id) {
+            return Content ($"id: {id}");
+        }
+        // PostMan
+        // http://localhost:5000/Home/FirstSample/custom-route?query=custom-query
+        // Headers(key:Value): {header:custom-header}
+        // Body(key:Value): x-www-form-urlencoded {form: custom-form} 
+        public IActionResult FirstSample (
+            [FromHeader] string header, [FromForm] string form, [FromRoute] string id, [FromQuery] string query) {
+            return Content ($"header: {header}, form: {form}, id: {id}, query: {query}");
         }
 
-        public string About () {
-            return "Hello About";
+        //http://localhost:5000/Home/DISample
+        public IActionResult DISample ([FromServices] ILogger<HomeController> logger) {
+            return Content ($"logger is null? {logger == null}");
         }
 
-        public string Test () {
-            return "Hello Test";
-        }
-
-        [HttpGet ("home/api1/{p1}")]
-        public string api1 (string p1) {
-            return p1;
-        }
-
-        public string api2 (string p1, string p2, string p3) {
-            return $"p1={p1}, p2={p2}, p3={p3}";
+        public IActionResult BodySample ([FromBody] UserModel model) {
+            // 由於 Id 是 int 型別，int 預設為 0
+            // 雖然有帶上 [Required]，但不是 null 所以算是有值。
+            if (model != null && model.Id < 1) {
+                ModelState.AddModelError ("Id", "Id not exist");
+            }
+            if (ModelState.IsValid) {
+                return Ok (model);
+            }
+            return BadRequest (ModelState);
         }
 
     }
+
 }
---
+++ MyMVC/Models/Attributes/AgeCheckAttribute.cs
@@ -0,0 +1,36 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyWebsite.Attributes {
+    public class AgeCheckAttribute : ValidationAttribute {
+        public int MinimumAge { get; private set; }
+        public int MaximumAge { get; private set; }
+
+        public AgeCheckAttribute (int minimumAge, int maximumAge) {
+            MinimumAge = minimumAge;
+            MaximumAge = maximumAge;
+        }
+
+        protected override ValidationResult IsValid (object value, ValidationContext validationContext) {
+            var date = Convert.ToDateTime (value);
+
+            if (date.AddYears (MinimumAge) > DateTime.Today ||
+                date.AddYears (MaximumAge) < DateTime.Today) {
+                return new ValidationResult (GetErrorMessage (validationContext));
+            }
+
+            return ValidationResult.Success;
+        }
+
+        private string GetErrorMessage (ValidationContext validationContext) {
+            // 有帶 ErrorMessage 的話優先使用
+            // [AgeCheck(18, 120, ErrorMessage="xxx")] 
+            if (!string.IsNullOrEmpty (this.ErrorMessage)) {
+                return this.ErrorMessage;
+            }
+
+            // 自訂錯誤訊息
+            return $"{validationContext.DisplayName} can't be in future";
+        }
+    }
+}
